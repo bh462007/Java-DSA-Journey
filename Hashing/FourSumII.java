a21d29421304ea575af45463c6f97eaf2d13f633
bh462007 <bh462007@users.noreply.github.com>
2026-06-04 23:46:00 +0530

    Four Sum II

---
+++ Hashing/FourSumII.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+class FourSumII {
+
+    static int fourSumCount(int[] nums1, int[] nums2,
+                            int[] nums3, int[] nums4) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int a : nums1) {
+            for (int b : nums2) {
+                int sum = a + b;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int count = 0;
+
+        for (int c : nums3) {
+            for (int d : nums4) {
+
+                int sum = c + d;
+                int target = -sum;
+
+                if (map.containsKey(target)) {
+                    count += map.get(target);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2};
+        int[] nums2 = {-2, -1};
+        int[] nums3 = {-1, 2};
+        int[] nums4 = {0, 2};
+
+        System.out.println(fourSumCount(nums1, nums2, nums3, nums4));
+    }
+}
